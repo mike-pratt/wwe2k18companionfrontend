fd4bdc6814f48275a549d9f7bae6db217e6ef729
Mike <mike.pratt@trelleborg.com>
2019-03-24 11:01:49 +0000

    Added missing properties to Championship model. Created championship service to connect to the apis championships controller endpoints. Added html template and code for the championships list component.

---
+++ src/app/shared/services/championships/championship.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+import { Paged } from '../../models/paged.model';
+import { BaseService } from '../base.service';
+import { Championship } from '../../models/championships/championship.model';
+
+@Injectable()
+export class ChampionshipService extends BaseService  {
+
+  constructor(_http: Http) {
+    super(_http);
+    this.actionUrl += 'v0/championship';
+  }
+
+  public getChampionships(pageNumber: number): Observable<Paged<Championship>> {
+    return this._http.get(this.actionUrl + '?page=' + pageNumber, this.getRequestOptions())
+        .map((data) => {
+          return data.json();
+        });
+  }
+
+  public getChampionshipById(id: number): Observable<Championship> {
+    return this._http.get(this.actionUrl + '/' + id, this.getRequestOptions())
+        .map((data) => {
+          return data.json();
+        });
+  }
+
+  public updateChampionship(championship: Championship): Observable<Response> {
+    return this._http.put(this.actionUrl + '/' + championship.id, JSON.stringify(championship), this.getRequestOptions());
+  }
+
+  public createChampionship(championship: Championship): Observable<Response> {
+    return this._http.post(this.actionUrl, JSON.stringify(championship), this.getRequestOptions());
+  }
+
+  public deleteChampionship(id: number): Observable<Response> {
+    return this._http.delete(this.actionUrl + '/' + id, this.getRequestOptions());
+  }
+
+}
