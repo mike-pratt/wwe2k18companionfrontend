05891835e7dcd608a62b5a395e19dbed67ed5a1d
Mike <mikep2419@gmail.com>
2019-04-07 18:36:40 +0100

    Re-added lost championships list component.

--- src/app/championships/championships-list/championships-list.component.ts
+++ src/app/championships/championships-list/championships-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Paged } from '../../shared/models/paged.model';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+import { Championship } from '../../shared/models/championships/championship.model';
+import { ChampionshipService } from '../../shared/services/championships/championship.service';
 
 @Component({
   selector: 'app-championships-list',
@@ -7,9 +12,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChampionshipsListComponent implements OnInit {
 
-  constructor() { }
+    public pageOffset = 0;
+    public columns = [
+        { name: 'Championship Name', prop: 'name' },
+    ];
 
-  ngOnInit() {
-  }
+    public championships: Paged<Championship>;
+
+    constructor(private _championshipService: ChampionshipService,
+                private _router: Router) {
+    }
+
+    ngOnInit() {
+        this.serviceGetChampionships(0);
+    }
+
+    public goToPage(event): void {
+        this.serviceGetChampionships(event.offset + 1);
+    }
+
+    public goToView(event: any): void {
+        const championship = event.selected[0];
+        this._router.navigate(['championships/view', championship.id]);
+    }
+
+    public createChampionship(show): void {
+        this.serviceCreateChampionship(show).add(() => {
+            this.serviceGetChampionships(this.pageOffset);
+        });
+    }
+
+    private serviceGetChampionships(page: number): Subscription {
+        return this._championshipService.getChampionships(page).subscribe((data: Paged<Championship>) => {
+            this.championships = data;
+        });
+    }
+
+    private serviceCreateChampionship(championship: Championship): Subscription {
+        return this._championshipService.createChampionship(championship).subscribe();
+    }
 
 }
